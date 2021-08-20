f9c583aa2b27a3bb2d5d9fdb8f4c047b8022f6c2
Adrian Sadowski <sadow.adrian@gmail.com>
2021-08-20 20:09:16 +0200

    error getAttribute data-id

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 {
   'use strict';
 
@@ -15,47 +16,56 @@
     bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
 
+  class BookList{
 
-  function render() {
-    //const thisBook = this;
+    constructor (){
+      const thisBook = this;
 
-    for (let eachBook of dataSource.books){
-      const generatedHTML = templates.bookTemplate(eachBook);
+      thisBook.render();
+      thisBook.initActions();
+    }
+    render() {
+      //const thisBook = this;
+
+      for (let eachBook of dataSource.books){
+        const generatedHTML = templates.bookTemplate(eachBook);
+        
+        // element dom from html
+        const element = utils.createDOMFromHTML(generatedHTML);
+        //console.log ('element:', element);
+
+        // genedated DOM add to list .books-list as new child
+        const bookListContainer = document.querySelector(select.containerOf.booksList);
+        //console.log('bookListContainer:', bookListContainer);
+
+        bookListContainer.appendChild(element);
+      }
+    }
+
+    initActions() {
+      const thisBook = this;
+      const favoriteBooks = [];
+      //const bookListContainer = document.querySelector(select.containerOf.booksList);
+      const booksImages = document.querySelectorAll(select.containerOf.images);
+      for(let image of booksImages){
+        //console.log('image', image);
+        image.addEventListener('click', function(event){
+          event.preventDefault();
+          image.classList.add('favorite');
+          const idBook = thisBook.booksList.getAttribute('data-id');
+          favoriteBooks.push(idBook);
+        });
+
+      }
       
-      // element dom from html
-      const element = utils.createDOMFromHTML(generatedHTML);
-      //console.log ('element:', element);
-
-      // genedated DOM add to list .books-list as new child
-      const bookListContainer = document.querySelector(select.containerOf.booksList);
-      //console.log('bookListContainer:', bookListContainer);
-
-      bookListContainer.appendChild(element);
+      console.log('favoriteBooks', favoriteBooks);
     }
-  }
-
-  function initActions() {
-    const thisBook = this;
-    const favoriteBooks = [];
-
-    const booksImages = document.querySelectorAll(select.containerOf.images);
-    for(let image of booksImages){
-      //console.log('image', image);
-      image.addEventListener('click', function(event){
-        event.preventDefault();
-        image.classList.add('favorite');
-        const idBook = thisBook.booksList.getAttribute('data-id');
-        favoriteBooks.push(idBook);
-      });
-    }
-    
-    console.log('favoriteBooks', favoriteBooks);
-    console.log('select.booksImages.images', booksImages);
-  }
-  // wywołanie funkcji
-  
-  render();
-  initActions();
+    // wywołanie funkcji
  
+  }
+
+// eslint-disable-next-line no-unused-vars
+const app = new BookList();
+
 
 }
